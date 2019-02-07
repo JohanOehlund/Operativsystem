efc27af8478f5f700a098857ba7116d8a99617b7
Johan Öhlund <c15jod@cs.umu.se>
2019-02-07 13:11:22 +0100

    fixed small thing in script

--- OU1/task.sh
+++ OU1/task.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
+# change directory to the location of this script
+cd "$(dirname "$0")"
+echo $0
 for i in {1..5}
 	do
-		dd if=/dev/zero of=/home/c15aen/Operativsystem/Operativsystem/OU1/test oflag=nocache oflag=dsync bs=1M count=500 &
+		dd if=/dev/zero of=test oflag=nocache,dsync bs=1M count=1 &
 	done
 
 
